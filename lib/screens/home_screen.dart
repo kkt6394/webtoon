3dd3e70b0421e762389616618c984d4a17ebc6f1
kkt6394 <kkt6394@gmail.com>
2025-09-03 21:36:03 +0900

    API

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../models/webtoon_model.dart';
+import '../services/api_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebtoons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {
+
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+}
