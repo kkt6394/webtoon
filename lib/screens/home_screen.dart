fa8ed5dcf865205770f4c0171d21f31bb87d8b39
kkt6394 <kkt6394@gmail.com>
2025-09-03 22:10:13 +0900

    FutureBuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,35 +3,13 @@ import 'package:flutter/material.dart';
 import '../models/webtoon_model.dart';
 import '../services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
-class _HomeScreenState extends State<HomeScreen> {
-
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {
-
-    });
-  }
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-
-  }
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -40,11 +18,21 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.white,
         title: Text(
           "오늘의 웹툰",
-          style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w400,
-          ),
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView(
+              children: [
+                for (var webtoon in snapshot.data!) Text(webtoon.title)
+              ],
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
